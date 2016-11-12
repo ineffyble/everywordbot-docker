11de0a1a71fa4f9b4916e4833865d00a433fcecc
Effy Elden <git@effy.is>
2016-11-13 04:29:28 +1100

    Hashbang#\!

--- tweet.sh
+++ tweet.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 echo "Tweeting $PREFIX`head -n \`cat /data/index.txt\` /data/words.txt | tail -n 1`$SUFFIX"
 python /app/everywordbot.py --consumer_key="$CONSUMER_KEY" --consumer_secret="$CONSUMER_SECRET" --access_token="$ACCESS_TOKEN" --token_secret="$TOKEN_SECRET" --source_file="/data/words.txt" --index_file="/data/index.txt" --prefix="$PREFIX" --suffix="$SUFFIX"
